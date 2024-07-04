271af77cf4b860a1325ec60c8dff9ffd4367adc8
Charles Eckman <charles@cceckman.com>
2024-07-04 16:22:59 -0400

    Parse an example JSON record into a database

---
+++ src/bin/crunch_local.rs
@@ -0,0 +1,15 @@
+use log_cruncher::Cruncher;
+use std::fs::File;
+use std::path::Path;
+
+fn main() {
+    let src: &Path = Path::new("quarantine/example.log.gz");
+    let db: &Path = Path::new("quarantine/example.db");
+
+    let src = File::open(src).expect("could not open example file");
+    let cruncher = Cruncher::new(db).expect("could not open DB");
+
+    cruncher
+        .crunch_gz(std::io::BufReader::new(src))
+        .expect("could not crunch records");
+}
