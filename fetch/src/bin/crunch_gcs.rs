29d338f8b880ca7d5979692b89775b8cf159a582
Charles Eckman <charles@cceckman.com>
2024-07-26 13:04:40 -0400

    Respect FD limit as a concurrency limit

--- fetch/src/bin/crunch_gcs.rs
+++ fetch/src/bin/crunch_gcs.rs
@@ -1,4 +1,7 @@
-use std::path::Path;
+use std::{
+    cmp::{max, min},
+    path::Path,
+};
 
 use log_cruncher::Cruncher;
 
@@ -15,13 +18,25 @@ fn main() {
         .enable_all()
         .build()
         .unwrap();
+
+    // Keep the concurrency well under the FD limit,
+    // so we don't run out of FDs for connections.
+    let (soft_fd_limit, hard_fd_limit) =
+        nix::sys::resource::getrlimit(nix::sys::resource::Resource::RLIMIT_NOFILE)
+            .expect("could not query FD limit");
+    tracing::debug!("FD limit of {soft_fd_limit} (soft) / {hard_fd_limit} (hard)");
+    let concurrency: usize = max(1, min(soft_fd_limit.saturating_sub(100), 1024))
+        .try_into()
+        .expect("could not fit concurrency limit into usize");
+
     Cruncher {
         gcs_path,
         database: dbfile.to_owned(),
         // This seems to be the limiting factor when cleanup is enabled.
         // Tokio will handle the thread count for us;
         // this is just a memory limit. And we have a lot of memory.
-        concurrency: 1024,
+        // We do have to keep it under the fd limit, though!
+        concurrency,
         // Not convinced I'm not losing logs to this, so far.
         cleanup: true,
     }
