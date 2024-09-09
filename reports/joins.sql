62407cc4812ba0d9e9208ad9bdf243fafbd0db02
Charles Eckman <charles@cceckman.com>
2024-09-08 20:56:30 -0400

    Add daily top articles, strip out some noise

--- reports/joins.sql
+++ reports/joins.sql
@@ -13,18 +13,26 @@ SELECT
 ,   paths.path as url_path
 ,   referers.referer as referer
 ,   user_agents.user_agent as user_agent
+,   date(requests.request_start_time) as date
 FROM
     requests
+    -- We don't have the same column name in the two tables,
+    -- so we can't NATURAL JOIN. Oh well.
     LEFT JOIN paths ON requests.url_path = paths.id
     LEFT JOIN referers ON requests.referer = referers.id
     LEFT JOIN user_agents ON requests.user_agent = user_agents.id
     LEFT JOIN autonomous_systems ON requests.asn = autonomous_systems.asn
 ;
 
--- and without.
-CREATE TEMP VIEW alltime AS
+-- without blackbox probes...
+CREATE TEMP VIEW alltime_allreq AS
 SELECT * FROM reqs
-WHERE reqs.user_agent NOT LIKE "%blackbox%";
+WHERE user_agent NOT LIKE "%blackbox%";
+
+-- ...and without spam traffic, where we can get rid of it.
+CREATE TEMP VIEW alltime AS
+SELECT * FROM alltime_allreq
+WHERE status != 404;
 
 -- Just the last week
 CREATE TEMP VIEW r AS
--- reports/summary.sql
+++ reports/summary.sql
@@ -3,14 +3,34 @@
 
 SELECT COUNT(*) as traffic FROM r;
 
-
 .print ''
-.print 'Top user agents:'
-SELECT substr(r.user_agent, 0, 70) top_agents, COUNT(*) as count
+.print 'Top articles:'
+SELECT substr(r.url_path, 0, 70) as top_articles, COUNT(*) as count
 FROM r
-GROUP BY r.user_agent
+WHERE r.url_path LIKE "%/writing/%/"
+GROUP BY r.url_path
 ORDER BY count DESC
 LIMIT 20;
+
+.print ''
+.print 'Top 3 articles by date:'
+WITH counts_by_date AS (
+    -- Sub-select: count of article-visits by date
+    SELECT 
+        substr(r.url_path, 0, 70) as top_articles
+    ,   COUNT(*) as count
+    ,   date
+    FROM r
+    WHERE r.url_path LIKE "%/writing/%/"
+    GROUP BY date, url_path
+)
+SELECT top_articles, count, date FROM (
+    SELECT *, row_number() over win1 as url_rank
+    FROM counts_by_date
+    WINDOW win1 AS (PARTITION BY date ORDER BY count DESC)
+)
+WHERE url_rank <= 3
+ORDER BY date, count DESC;
 
 .print ''
 .print 'Top referers:'
@@ -18,8 +38,17 @@ SELECT substr(r.referer, 0, 70) as top_referer, COUNT(*) as count
 FROM r
 WHERE
     r.referer NOT NULL
+AND r.referer != ""
 AND r.referer NOT LIKE "%cceckman.com%"
 GROUP BY r.referer
+ORDER BY count DESC
+LIMIT 20;
+
+.print ''
+.print 'Top user agents:'
+SELECT substr(r.user_agent, 0, 70) top_agents, COUNT(*) as count
+FROM r
+GROUP BY r.user_agent
 ORDER BY count DESC
 LIMIT 20;
 
@@ -31,14 +60,7 @@ GROUP BY r.url_path
 ORDER BY count DESC
 LIMIT 20;
 
-.print ''
-.print 'Top articles:'
-SELECT substr(r.url_path, 0, 70) as top_articles, COUNT(*) as count
-FROM r
-WHERE r.url_path LIKE "%/writing/%/"
-GROUP BY r.url_path
-ORDER BY count DESC
-LIMIT 20;
+
 
 .print ''
 .print 'Top errors:'
